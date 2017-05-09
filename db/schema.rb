ef598db43dce1574917eb884adbe383e15f3f130
glz <glzztt@gmail.com>
2017-05-09 21:12:37 +0800

    Add User username_lower attribute

--- db/schema.rb
+++ db/schema.rb
@@ -18,11 +18,12 @@ ActiveRecord::Schema.define(version: 20170506030707) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
+    t.string   "username_lower"
     t.string   "name"
     t.string   "email"
     t.text     "bio"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,8 @@ class UserTest < ActiveSupport::TestCase
   test "should create user" do
     assert_not_nil create(:user)
   end
+
+  test "should have lower username" do
+    assert_equal "lower", create(:user, username: 'LOWER').username_lower
+  end
 end
