d3e5da40092e698d4876b0de0b7ae8d11cb2ed73
glz <glzztt@gmail.com>
2017-06-24 06:07:57 +0800

    Session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'topics#index'
   get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  post 'login', to: 'sessions#create'
   resources :users, only: [:create] do
     collection do
       get :check_email
