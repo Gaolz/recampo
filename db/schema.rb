f632ac1129120c7720e07c610c21b4692b557613
glz <glzztt@gmail.com>
2017-06-24 08:13:54 +0800

    Init Topic and Post model

--- db/schema.rb
+++ db/schema.rb
@@ -11,21 +11,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170506030707) do
+ActiveRecord::Schema.define(version: 20170624000649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "posts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "post_number"
+    t.integer  "topic_id"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "posts", ["topic_id"], name: "index_posts_on_topic_id", using: :btree
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "topics", ["user_id"], name: "index_topics_on_user_id", using: :btree
+
   create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "username_lower"
     t.string   "name"
     t.string   "email"
     t.string   "email_lower"
-    t.string   "username"
-    t.string   "username_lower"
     t.string   "password_digest"
     t.text     "bio"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "posts", "topics"
+  add_foreign_key "posts", "users"
+  add_foreign_key "topics", "users"
 end
