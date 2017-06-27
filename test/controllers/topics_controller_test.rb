1936d8054e9ec77f73da0889eb64568dedd476d0
glz <glzztt@gmail.com>
2017-06-27 11:27:57 +0800

    New topic button

--- test/controllers/topics_controller_test.rb
+++ test/controllers/topics_controller_test.rb
@@ -2,8 +2,9 @@ require 'test_helper'
 
 class TopicsControllerTest < ActionController::TestCase
   test "should get index" do
+    3.times { create(:topic) }
     get :index
-    assert_response :success
+    assert_response :success, @response.body
   end
 
 end
