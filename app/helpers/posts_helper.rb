289c1d89d93ab8f5029db072547a9e21eb68bb6e
glz <glzztt@gmail.com>
2017-08-15 16:56:10 +0800

    Update post vote status in js

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,7 @@
 module PostsHelper
+    def post_votes(posts, user)
+        usr.post_votes.where(post_id: posts.pluck(:id)).map { |post_vote| 
+            { post_id: post_vote.post_id, value: post_vote.value }
+        }
+    end
 end
